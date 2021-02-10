ebb2524879e8b42098799b5796df13d4c3a40a47
Novus Edge <novusedge0@gmail.com>
2021-02-10 06:37:06 +0530

    Added the loading screen

--- Bot/cmds/get_kawaii.py
+++ Bot/cmds/get_kawaii.py
@@ -1,10 +1,36 @@
-import requests, json, discord
+import requests, json, discord, time
 
 async def kawaii(ctx):
-    kawaiiGif = requests.get( "https://api.giphy.com/v1/gifs/random?api_key=LSN3t1vDsXlV40noyN06kEGlBopLsZNQ&tag=kawaii&rating=g" )
-    kawaiiGif = kawaiiGif.json()["data"]["images"]["original"]["url"]
+    loadingGif = requests.get( "https://api.giphy.com/v1/gifs/random?api_key=LSN3t1vDsXlV40noyN06kEGlBopLsZNQ&tag=kawaii-loading-anime&rating=g" )
+    loadingGif = loadingGif.json()["data"]["images"]["original"]["url"]
+    emObj = discord.Embed.from_dict({
+    "embed": {
+        "title": "Generating Cuteness...",
+        "image": {
+            "url": f"{loadingGif}"
+        },
+        "color": 1035781,
+        "footer": {
+            "text": "by UnderBot"
+        },
+        "author": {
+            "name": "UnderBot",
+            "icon_url": "https://raw.githubusercontent.com/NovusEdge/UnderBot/master/logo/logo(pexels-sebastiaan-stam-1097456).ico"
+        },
+        "fields": [ ]
+    }
+    }["embed"])
 
-    embedObj = discord.Embed.from_dict({
+    waitmsg = await ctx.send(embed=emObj)
+
+    try:
+        kawaiiGif = requests.get( "https://api.giphy.com/v1/gifs/random?api_key=LSN3t1vDsXlV40noyN06kEGlBopLsZNQ&tag=kawaii&rating=g" )
+        kawaiiGif = kawaiiGif.json()["data"]["images"]["original"]["url"]
+
+        time.sleep(2)
+        await waitmsg.delete()
+
+        embedObj = discord.Embed.from_dict({
           "content": "Dog: ",
             "embed": {
                 "color": 40447,
@@ -21,5 +47,10 @@ async def kawaii(ctx):
                 },
         "fields": [ ]
         }
-    }['embed'])
-    await ctx.send(embed=embedObj)
+        }['embed'])
+        await ctx.send(embed=embedObj)
+
+    except:
+        time.sleep(2)
+        await waitmsg.delete()
+        await ctx.send("`Error generating cuteness :cry:`")
