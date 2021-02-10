ac5964c37c14ba38547fc53cde4e0bae45045006
Novus Edge <novusedge0@gmail.com>
2021-02-10 06:37:31 +0530

    Added a command to change prefix

--- Bot/cmds/set_pre.py
+++ Bot/cmds/set_pre.py
@@ -3,14 +3,13 @@ import discord, json, re
 async def setprefix(ctx, botObj):
     try:
         pre = get_prefix(ctx)
+        print(pre)
         botObj.command_prefix = pre
 
-        ctx.send(f"`Command Prefix` changed to `{pre}`")
+        await ctx.send(f"`Command Prefix` changed to `{pre}`")
     except:
-        ctx.send(r"`Error in changing Command Prefix :(`")
+        await ctx.send(r"`Error in changing Command Prefix :(`")
 
 def get_prefix(ctx):
     msg = ctx.message.content
-    matches = re.findall(r'''(["'])(?:(?=(\\?))\2.)*?\1''', msg)
-
-    return matches[0]
+    return msg.partition("prefix=")[2][1:-1]
